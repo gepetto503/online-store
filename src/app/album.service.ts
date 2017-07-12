0a243bf14ea45cb4ac676d290c9c74c2d6798837
Brendan Hanna and Dylan Dills <brendan+dylan@>
2017-07-12 16:22:44 -0700

    add comments to code for clarity

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -10,15 +10,17 @@ export class AlbumService {
   //sets up a var with the right type to hold our albums data from firebase
   albums: FirebaseListObservable<any[]>;
 
-  //actually sets albums var to the data from firebase
+  //when this class is instantiated, it comes with our Firebase database and it sets the albums var to the list of albums from firebase
   constructor(private database: AngularFireDatabase) {
     this.albums = database.list('albums');
   }
 
+  //this is a method we can call whenever we want access to the albums list from firebase
   getAlbums() {
     return this.albums;
   }
 
+  //this method pushes a new album object to the database
   addAlbum(newAlbum: Album) {
     this.albums.push(newAlbum);
   }
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -12,12 +12,18 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 
 export class MarketplaceComponent implements OnInit {
+  //sets up a variable ready to receive an object from firebase,
+  //ultimately fed to it by the getAlbums method in the AlbumService injectable
   albums: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
 
+  //this constructor has two... properties? router and albumService.  router is an instance of the router, so this component has access to a router and knows what it means.  albumService is an instance of the AlbumService class from that service, so this component has access to stuff in there, too.
   constructor(private router: Router, private albumService: AlbumService){}
 
+  //whenever an object of this class is initialized...
   ngOnInit(){
+    //set the albums property, which is a FirebaseListObservable file type...
+    //albumService is injected from the service, so we have access to its getAlbums method, which just return an album object from firebase.
     this.albums = this.albumService.getAlbums();
   }
 
